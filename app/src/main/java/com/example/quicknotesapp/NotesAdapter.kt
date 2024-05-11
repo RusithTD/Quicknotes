ab855876ddd6ffab48a1d75cb8e9b9e16360e091
RusithTD <it22188854@my.sliit.lk>
2024-05-11 11:27:16 +0530

    Update functionality

--- app/src/main/java/com/example/quicknotesapp/NotesAdapter.kt
+++ app/src/main/java/com/example/quicknotesapp/NotesAdapter.kt
@@ -1,9 +1,11 @@
 package com.example.quicknotesapp
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
@@ -14,6 +16,7 @@ class NotesAdapter(private var notes: List<Note>, private val context: Context) 
     class NoteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val titleTextView: TextView = itemView.findViewById(R.id.titleTextview)
         val contentTextView: TextView = itemView.findViewById(R.id.contentTextView)
+        val updateButton:ImageView = itemView.findViewById(R.id.updateButton)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
@@ -29,6 +32,13 @@ class NotesAdapter(private var notes: List<Note>, private val context: Context) 
         val note = notes[position]
         holder.titleTextView.text = note.title
         holder.contentTextView.text = note.content
+
+        holder.updateButton.setOnClickListener{
+            val intent = Intent(holder.itemView.context, UpdateNoteActivity::class.java).apply {
+                putExtra("note_id",note.id)
+            }
+            holder.itemView.context.startActivity(intent)
+        }
     }
 
     fun refreshData(newNotes: List<Note>) {
